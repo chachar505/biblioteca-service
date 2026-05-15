7e24f57fe05df95dcbaaafd6048ab4f8e07cbf24
alaburtoc <al.aburtoc@duocuc.cl>
2026-05-15 16:57:40 -0400

    finalizado

--- src/main/java/com/gameup/library_service/client/JuegoFeignClient.java
+++ src/main/java/com/gameup/library_service/client/JuegoFeignClient.java
@@ -1,4 +1,13 @@
 package com.gameup.library_service.client;
 
-public class JuegoFeignClient {
+import com.gameup.library_service.dto.JuegoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "juego-service", url = "${juego.service.url}")
+public interface JuegoFeignClient {
+
+    @GetMapping("/juegos/{id}")
+    JuegoDTO obtenerJuegoPorId(@PathVariable("id") Long id);
 }
--- src/main/java/com/gameup/library_service/dto/BibliotecaRequestDTO.java
+++ src/main/java/com/gameup/library_service/dto/BibliotecaRequestDTO.java
@@ -1,7 +1,16 @@
 package com.gameup.library_service.dto;
 
+import com.gameup.library_service.model.EstadoJuego;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BibliotecaRequestDTO {
-    public String getIdUsuario() {
-        return null;
-    }
+
+    private Long idUsuario;
+    private Long idJuego;
+    private EstadoJuego estado;
 }
